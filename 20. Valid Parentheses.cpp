2688d2e3d5f7b14e8f8453a942f565bf711f2646
alecwu44743 <alec.wu44743@gmail.com>
2022-07-24 20:05:06 +0800

    20. Valid Parentheses

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,39 @@
+// Leetcode Link: https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        
+        for(int i=0; i<s.size(); i++){
+            char c = s[i];
+            
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else if(c == ')' || c == '}' || c == ']'){
+                if(st.empty()){
+                    st.push(c);
+                    break;
+                }
+                
+                char tp = st.top(); st.pop();
+                if(tp == '(' && c ==')'){
+                    continue;
+                }
+                else if(tp == '{' && c =='}'){
+                    continue;
+                }
+                else if(tp == '[' && c ==']'){
+                    continue;
+                }
+                else{
+                    st.push(c);
+                    break;
+                }
+            }
+        }
+        
+        return st.empty();
+    }
+};
